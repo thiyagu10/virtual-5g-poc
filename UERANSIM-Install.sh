6f3aa9996982ac410d6cbde9996517e471039434
thiyagu10 <thiyaguitx@gmail.com>
2022-10-06 10:44:50 +0530

    Update UERANSIM-Install.sh

--- UERANSIM-Install.sh
+++ UERANSIM-Install.sh
@@ -1,22 +1,35 @@
 #################################################################################
 #				UERANSIM Compilation and Installation Steps						#
 #################################################################################
-cd ~
-git clone https://github.com/aligungr/UERANSIM
+RANBLDDIR=$HOME/NRRANBLD
+
 sudo apt update
 sudo apt upgrade
 sudo apt install make gcc g++ libsctp-dev lksctp-tools iproute2
 sudo snap install cmake --classic
-cd ~/UERANSIM
-make
+
+mkdir -pv $RANBLDDIR
+cd $RANBLDDIR
+git clone https://github.com/aligungr/UERANSIM
+cd UERANSIM
+make -j $(nproc)
+if [ -f "${RANBLDDIR}/UERANSIM/build/nr-gnb" -a -f "${RANBLDDIR}/UERANSIM/build/nr-ue" ]
+then
+	echo "UERANSIM NR-gNB and NR-UE are compiled successfully"
+else
+	echo "Probelm in compiling the UERANSIM NR-gNB and NR-UE"
+fi
+
+cp ${RANBLDDIR}/UERANSIM/config/
+
 cat <<EOF | sudo tee /etc/systemd/system/nr-gnb01.service
 [Unit]
 Description = UERANSIM NR gNB, RF Over RLS-3.2
 After = network.target
 
 [Service]
-WorkingDirectory=/root/UERANSIM/build
-ExecStart=/root/UERANSIM/build/nr-gnb -c ../config/gnb01.yaml
+WorkingDirectory=${RANBLDDIR}/UERANSIM/build
+ExecStart=${RANBLDDIR}/UERANSIM/build/nr-gnb -c ../config/gnb01.yaml
 
 [Install]
 WantedBy = multi-user.target
